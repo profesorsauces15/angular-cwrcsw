daebe1120b8799d2837ffc49938bba055c49be38
profesorsauces15 <profesorsauces15@gmail.com>
2020-04-28 13:51:06 +0200

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -23,6 +23,8 @@ export const products = [
 
 
 /*
+
+Con Alex Dominguez
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at http://angular.io/license
